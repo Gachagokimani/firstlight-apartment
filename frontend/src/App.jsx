5a06449b997b0c34d39e51d26e0746e146fced09
Gachagokimani <gachagokimani@gmail.com>
2025-02-22 18:41:28 +0300

    landing page edit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import  { Suspense, lazy } from "react";
 
 // Lazy load components
@@ -12,6 +12,7 @@ function App() {
     <Router>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
+          <Route path="/" element={<Navigate to="/LandingPage" />} />
           <Route path="/LandingPage" element={<LandingPage />} />
           <Route path="/LoginForm" element={<LoginForm />} />
           <Route path="/SearchHomes" element={<SearchHomes />} />
