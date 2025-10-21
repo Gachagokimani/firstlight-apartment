9d7c5374fca5074946bf401a3837423f22a3f1ee
Gachagokimani <gachagokimani@gmail.com>
2025-10-21 22:19:29 +0300

    route & password reset functionality

--- frontend/src/components/signup/signup.jsx
+++ frontend/src/components/signup/signup.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import './signupForm.css';
+import './SignupForm.css';
 
 const SignupForm = ({ onSignup }) => {
   const [formData, setFormData] = useState({
